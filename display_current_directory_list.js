85c5579c136886bbb895d7fc67b42cfeaa67a0ec
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-12-10 21:53:05 +0900

    [Update] 同一フォルダのファイル一覧の表示を整理
    横幅が不十分だと1ファイルが複数行になってしまう

--- display_current_directory_list.js
+++ display_current_directory_list.js
@@ -7,9 +7,10 @@
 	const iframe = document.createElement("iframe");
 	iframe.style.position = "fixed";
 	iframe.style.top = "0px";
-	iframe.style.right = "0px";
+	// 右側30%に表示する
+	iframe.style.left = "70%";
 	iframe.style.height = "100%";
-	iframe.style.width = "500px";
+	iframe.style.width = "100%";
 	iframe.style.background = "white";
 	iframe.src = document.URL.replace(/[/][^/]+$/, "?in-iframe");
 	document.body.appendChild(iframe);
