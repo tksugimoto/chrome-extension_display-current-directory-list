4529422d1f0dd616acbeb650745a466381a3288d
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-04-23 22:41:04 +0900

    [Add #1] 「同一フォルダ内のファイル一覧を閉じる」メニューを右クリックメニューに追加

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,4 +1,31 @@
 
+const ID_CLOSE_DIRECTORY_LIST = "close-directory-list";
+
+const createContextMenu = () => {
+	chrome.contextMenus.create({
+		title: "同一フォルダ内のファイル一覧を閉じる",
+		contexts: [
+			"all"
+		],
+		documentUrlPatterns: [
+			"file:///*"
+		],
+		id: ID_CLOSE_DIRECTORY_LIST
+	});
+};
+
+chrome.runtime.onInstalled.addListener(createContextMenu);
+chrome.runtime.onStartup.addListener(createContextMenu);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+	if (info.menuItemId === ID_CLOSE_DIRECTORY_LIST) {
+		const message = {
+			method: "close-directory-list"
+		};
+		chrome.tabs.sendMessage(tab.id, message);
+	}
+});
+
 chrome.runtime.onMessage.addListener((message, sender) => {
 	if (message.method === "close-this-directory-list") {
 		const message = {
